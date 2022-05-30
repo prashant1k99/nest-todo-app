1da4cada1b0aba28497f9eac361f5ecde006a2c0
Prashant <prashantco111@gmail.com>
2022-05-30 22:53:52 +0530

    WIP: Added Mongo to App

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017,localhost:27018,localhost:27019?replicaSet=rs',
+    ),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
